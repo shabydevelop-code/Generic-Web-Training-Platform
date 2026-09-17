eb79f5ab1e2698f49e16c523753ec9003e20e568
shabydevelop-code <shaby.develop@gmail.com>
2026-09-17 23:58:56 +0300

    Handle training runner messages in content script

--- extension/content/content-script.js
+++ extension/content/content-script.js
@@ -7,11 +7,23 @@ chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
   if (message?.type === "GWTP_CLEAR_HIGHLIGHT") {
     stopElementPicker();
     clearHighlight();
+    clearTrainingStep();
     sendResponse({ success: true });
     return;
   }
 
   if (message?.type === "GWTP_START_ELEMENT_PICKER") {
     sendResponse(startElementPicker());
+    return;
+  }
+
+  if (message?.type === "GWTP_SHOW_TRAINING_STEP") {
+    sendResponse(showTrainingStep(message.step));
+    return;
+  }
+
+  if (message?.type === "GWTP_CLEAR_TRAINING_STEP") {
+    clearTrainingStep();
+    sendResponse({ success: true });
   }
 });
