74fb7b36facd4719d4ab3b1c61c6bbd7711f39a9
shabydevelop-code <shaby.develop@gmail.com>
2026-09-19 14:17:07 +0300

    Do not resume training automatically after login

--- extension/sidepanel/sidepanel.js
+++ extension/sidepanel/sidepanel.js
@@ -1261,10 +1261,6 @@ async function handleLogin() {
   await loadTopics();
   await loadGuides();
   await loadLearnerCatalog();
-
-  if (window.authService.getCurrentRole() === "learner") {
-    await window.guideRunner.restoreActiveStep();
-  }
 }
 
 async function handleLogout() {
