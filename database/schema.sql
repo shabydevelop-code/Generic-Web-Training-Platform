f7c4be69608d3385f0bdc2b83b64c9c493054bf2
shabydevelop-code <shaby.develop@gmail.com>
2026-09-21 00:44:13 +0300

    Add screen name to guide step schema

--- database/schema.sql
+++ database/schema.sql
@@ -35,6 +35,7 @@ CREATE TABLE IF NOT EXISTS GuideSteps (
     StepOrder INTEGER NOT NULL CHECK (StepOrder > 0),
     Selector TEXT NOT NULL,
     Instruction TEXT NOT NULL,
+    ScreenName TEXT,
     FrameTarget TEXT,
     ValidationEngine TEXT,
     ValidationExpression TEXT,
