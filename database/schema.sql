fa2225096deb5ff88e7ae34e2c8f5500d631fdc4
shabydevelop-code <shaby.develop@gmail.com>
2026-09-22 21:01:29 +0300

    Add interaction type to guide step schema

--- database/schema.sql
+++ database/schema.sql
@@ -36,6 +36,7 @@ CREATE TABLE IF NOT EXISTS GuideSteps (
     Selector TEXT NOT NULL,
     Instruction TEXT NOT NULL,
     ScreenName TEXT,
+    InteractionType TEXT NOT NULL DEFAULT 'auto',
     FrameTarget TEXT,
     ValidationEngine TEXT,
     ValidationExpression TEXT,
