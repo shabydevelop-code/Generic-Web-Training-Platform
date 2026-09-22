f4912b65c23cd65d2d8c7e1eb17aa63269593aa6
shabydevelop-code <shaby.develop@gmail.com>
2026-09-22 20:53:50 +0300

    Strengthen editor element highlight

--- extension/content/overlay/highlighter.js
+++ extension/content/overlay/highlighter.js
@@ -5,11 +5,18 @@ function clearHighlight() {
   const highlightedElements = document.querySelectorAll(`[${HIGHLIGHT_ATTRIBUTE}]`);
 
   highlightedElements.forEach((element) => {
-    element.style.outline = element.dataset.gwtpPreviousOutline || "";
-    element.style.outlineOffset = element.dataset.gwtpPreviousOutlineOffset || "";
+    const previousOutline = element.dataset.gwtpPreviousOutline || "";
+    const previousOutlinePriority = element.dataset.gwtpPreviousOutlinePriority || "";
+    const previousOutlineOffset = element.dataset.gwtpPreviousOutlineOffset || "";
+    const previousOutlineOffsetPriority = element.dataset.gwtpPreviousOutlineOffsetPriority || "";
+
+    element.style.setProperty("outline", previousOutline, previousOutlinePriority);
+    element.style.setProperty("outline-offset", previousOutlineOffset, previousOutlineOffsetPriority);
 
     delete element.dataset.gwtpPreviousOutline;
+    delete element.dataset.gwtpPreviousOutlinePriority;
     delete element.dataset.gwtpPreviousOutlineOffset;
+    delete element.dataset.gwtpPreviousOutlineOffsetPriority;
     element.removeAttribute(HIGHLIGHT_ATTRIBUTE);
   });
 }
@@ -28,11 +35,13 @@ function highlightElement(selector) {
   }
 
   const element = result.element;
-  element.dataset.gwtpPreviousOutline = element.style.outline;
-  element.dataset.gwtpPreviousOutlineOffset = element.style.outlineOffset;
+  element.dataset.gwtpPreviousOutline = element.style.getPropertyValue("outline");
+  element.dataset.gwtpPreviousOutlinePriority = element.style.getPropertyPriority("outline");
+  element.dataset.gwtpPreviousOutlineOffset = element.style.getPropertyValue("outline-offset");
+  element.dataset.gwtpPreviousOutlineOffsetPriority = element.style.getPropertyPriority("outline-offset");
   element.setAttribute(HIGHLIGHT_ATTRIBUTE, "true");
-  element.style.outline = `3px solid ${GWTP_HIGHLIGHT_ACCENT}`;
-  element.style.outlineOffset = "3px";
+  element.style.setProperty("outline", `3px solid ${GWTP_HIGHLIGHT_ACCENT}`, "important");
+  element.style.setProperty("outline-offset", "3px", "important");
   element.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
 
   return { success: true, message: "Element highlighted successfully." };
